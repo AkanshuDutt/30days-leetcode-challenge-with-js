e35f361504a1706a71626f86de83392b54f2256a
Akanshu_dutt <duttakanshu@gmail.com>
2025-10-27 15:12:41 +0530

    to be  or not to be problem in js

---
+++ Leetcode Problems/TobeorNotTobe.js
@@ -0,0 +1,35 @@
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual === expected) {
+        return true;
+      } else {
+        throw new Error("Not Equal"); // fixed extra space
+      } 
+    },
+    notToBe(expected) {
+      if (actual !== expected) {
+        return true;
+      } else {
+        throw new Error("Equal"); 
+      }
+    }
+  };
+}
+
+
+console.log(expect(5).toBe(5));        
+console.log(expect(5).notToBe(10));    
+
+
+try {
+  expect(5).toBe(10);       // Error: Not Equal
+} catch (err) {
+  console.log(err.message);
+}
+
+try {
+  expect(5).notToBe(5);     // Error: Equal
+} catch (err) {
+  console.log(err.message);
+}
