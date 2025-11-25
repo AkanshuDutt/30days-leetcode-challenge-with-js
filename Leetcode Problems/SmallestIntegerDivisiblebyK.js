95a19bf30a6322a8ed564774404f34bd85d6bbf7
Akanshu Dutt <duttakanshu@gmail.com>
2025-11-25 11:08:50 +0530

    Update SmallestIntegerDivisiblebyK.js

--- Leetcode Problems/SmallestIntegerDivisiblebyK.js
+++ Leetcode Problems/SmallestIntegerDivisiblebyK.js
@@ -1,1 +1,15 @@
-
+/**
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRepunitDivByK = function(k) {
+    if (K%2 == 0 || K % 5 ==0 ) return -1;
+    var r=0;
+    for( Var N = 1. N <=K; N++)
+    {
+        r=(r*10+1)%K;
+        if (r==0) Return N;
+    }
+    return -1;
+    
+};
